71eee2c15deb1450fae56c00e1c2804c71280379
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-15 15:16:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter][:christmas] << drinks
   holiday_hash[:winter][:new_years] <<drinks
-  
+
 end
 
 
