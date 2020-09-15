a2ac94b3771b90450f6ea5e5b278dcfced451d0a
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-15 15:11:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     season.each do |holiday, supply|
+      if holiday == "winter"
       holiday << "drinks"
     end
   end
