dc668d82caa077905ae3fb5687c1815e5d9dfa5e
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-15 15:22:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   return holiday_hash[:spring][:memorial_day]
 
   holiday_hash[:spring][:memorial_day] << "Table Cloth"
-  return holiday_hash[:spring][:memorial_day] end
+  return holiday_hash[:spring][:memorial_day]
+end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
